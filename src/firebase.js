9bc72df300a72ca9189d6be3e7a032d692141bd5
김태영 <kimtaeyeong@gimtaeyeong-ui-MacBookPro.local>
2020-09-28 11:41:23 +0900

    #1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6mGdje_CZzpQyXgQ8PtnZ_9ZbN-XXZvU",
+  authDomain: "nwitter-29ecf.firebaseapp.com",
+  databaseURL: "https://nwitter-29ecf.firebaseio.com",
+  projectId: "nwitter-29ecf",
+  storageBucket: "nwitter-29ecf.appspot.com",
+  messagingSenderId: "165270614174",
+  appId: "1:165270614174:web:06baf0863f294709954b8f",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
